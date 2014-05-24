91237cfb73b452a05931d97c6441775cd12df238
TXMaster <trashmail321>
2014-05-24 18:48:39 +0300

    Adding LoadingBiew  first ReactiveUI application

---
+++ FlickerSearcher/ViewModels/LoginViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace FlickerSearcher.ViewModels
+{
+    public class LoginViewModel : ReactiveObject
+    {
+
+
+        public ICommand LogInCommand
+        {
+            get; 
+            set;
+        }
+
+
+        private string mUserName;
+        
+        private string mPassword;
+
+        private string mLogInTime;
+
+        
+        public string UserName
+        {
+            get { return mUserName; }
+            set { this.RaiseAndSetIfChanged(ref mUserName, value); }
+        }
+
+        
+        public string Password
+        {
+            get { return mPassword; }
+           set{ this.RaiseAndSetIfChanged(ref mPassword, value); }
+        }
+
+
+        public string LogInTime {
+            get { return mLogInTime; }
+            set { this.RaiseAndSetIfChanged(ref mLogInTime, value); }}
+
+
+        public LoginViewModel()
+        {
+
+            var AreArgsValid = this.WhenAny(viewModel => viewModel.UserName, vm => vm.Password, (userName, password) =>
+                String.IsNullOrEmpty(userName.Value) &&
+                                                                                                                        String.IsNullOrEmpty(password.Value));
+
+
+         
+
+            LogInCommand = new ReactiveCommand(AreArgsValid);
+
+            var logInclient = ((ReactiveCommand)LogInCommand).Timestamp().Subscribe(
+
+               newUser =>
+                   LogInTime = newUser.Timestamp.ToString("t"));
+
+        }
+
+        
+    }
+}
---
+++ LoadingSample/LoginViewModel.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Input;
+using ReactiveUI;
+
+
+
+
+namespace LoadingSample
+{
+    public class LoginViewModel : ReactiveObject
+    {
+
+        private string mLoginTime;
+
+        private string mUsername;
+
+        private string mPassword;
+
+        
+
+
+        public string LogInTime
+        {
+            get { return mLoginTime; }
+
+            set { this.RaiseAndSetIfChanged(ref mLoginTime, value); }
+    }
+
+        public string Username
+        {
+            get { return mUsername; }
+
+            set { this.RaiseAndSetIfChanged(ref mUsername, value); }
+        }
+
+        public string Password
+        {
+            get { return mPassword; }
+
+            set { this.RaiseAndSetIfChanged(ref mPassword, value); }
+        }
+
+
+        
+        public ICommand LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            var AreArgsValid = this.WhenAny(vm => vm.Username,vm => vm.Password,(user,password) => !String.IsNullOrEmpty(user.Value) && !String.IsNullOrEmpty(password.Value));
+            LoginCommand = new ReactiveCommand(AreArgsValid);
+
+            var logInclient = ((ReactiveCommand)LoginCommand).Timestamp().Subscribe(
+
+               newUser =>
+                   LogInTime = newUser.Timestamp.ToString("T"));
+
+
+
+
+        }
+
+    }
+}
